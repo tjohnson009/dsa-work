d723d82a00ae5f9fbf67dad325dd896e9cab72d6
Tim Johnson <62769082+tjohnson009@users.noreply.github.com>
2024-08-27 19:07:31 -0400

    Finish final solution

--- mergeSort.js
+++ mergeSort.js
@@ -55,18 +55,18 @@ if (isSorted(right)) {
         return sorted
 }
 
-
-// console.log('ANSWER', mergeSort([3, 2, 1, 13, 8, 17, 2096, 45, 998, 5, 0, 200, 1])); 
-// console.log('ANSWER', mergeSort([12, 4, 7, 1, 9, 3, 5, 10])); // Expected: [1, 3, 4, 5, 7, 9, 10, 12]
-// console.log('ANSWER', mergeSort([14, 18, 22, 13, 7, 4, 15, 9, 5, 19, 2, 8, 1, 11, 20])); // Expected: [1, 2, 4, 5, 7, 8, 9, 11, 13, 14, 15, 18, 19, 20, 22]
-// console.log('ANSWER', mergeSort([9, 5, 2, 8, 4, 7, 3, 6, 1, 10])); // Expected: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-// console.log('ANSWER', mergeSort([3, 8, 4, 7, 1, 6, 2, 5, 9, 0])); // Expected: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-// console.log('ANSWER', mergeSort([25, 17, 8, 12, 30, 3, 6, 14, 18, 21, 19])); // Expected: [3, 6, 8, 12, 14, 17, 18, 19, 21, 25, 30]
-// console.log('ANSWER', mergeSort([12, 4, 7, 1, 9, 3, 5, 10])); // Expected: [1, 3, 4, 5, 7, 9, 10, 12]
-// console.log('ANSWER', mergeSort([14, 18, 22, 13, 7, 4, 15, 9, 5, 19, 2, 8, 1, 11, 20])); // Expected: [1, 2, 4, 5, 7, 8, 9, 11, 13, 14, 15, 18, 19, 20, 22]
-// console.log('ANSWER', mergeSort([9, 5, 2, 8, 4, 7, 3, 6, 1, 10])); // Expected: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-// console.log('ANSWER', mergeSort([3, 8, 4, 7, 1, 6, 2, 5, 9, 0])); // Expected: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-// console.log('ANSWER', mergeSort([25, 17, 8, 12, 30, 3, 6, 14, 18, 21, 19])); // Expected: [3, 6, 8, 12, 14, 17, 18, 19, 21, 25, 30]
-console.log('ANSWER', mergeSort([1,2,3,4,0,5,6,7])); // Expected: 1, 2, 3, 4, 5, 6, 7]
+// test cases 
+    // console.log('ANSWER', mergeSort([3, 2, 1, 13, 8, 17, 2096, 45, 998, 5, 0, 200, 1])); 
+    // console.log('ANSWER', mergeSort([12, 4, 7, 1, 9, 3, 5, 10])); // Expected: [1, 3, 4, 5, 7, 9, 10, 12]
+    // console.log('ANSWER', mergeSort([14, 18, 22, 13, 7, 4, 15, 9, 5, 19, 2, 8, 1, 11, 20])); // Expected: [1, 2, 4, 5, 7, 8, 9, 11, 13, 14, 15, 18, 19, 20, 22]
+    // console.log('ANSWER', mergeSort([9, 5, 2, 8, 4, 7, 3, 6, 1, 10])); // Expected: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    // console.log('ANSWER', mergeSort([3, 8, 4, 7, 1, 6, 2, 5, 9, 0])); // Expected: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    // console.log('ANSWER', mergeSort([25, 17, 8, 12, 30, 3, 6, 14, 18, 21, 19])); // Expected: [3, 6, 8, 12, 14, 17, 18, 19, 21, 25, 30]
+    // console.log('ANSWER', mergeSort([12, 4, 7, 1, 9, 3, 5, 10])); // Expected: [1, 3, 4, 5, 7, 9, 10, 12]
+    // console.log('ANSWER', mergeSort([14, 18, 22, 13, 7, 4, 15, 9, 5, 19, 2, 8, 1, 11, 20])); // Expected: [1, 2, 4, 5, 7, 8, 9, 11, 13, 14, 15, 18, 19, 20, 22]
+    // console.log('ANSWER', mergeSort([9, 5, 2, 8, 4, 7, 3, 6, 1, 10])); // Expected: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    // console.log('ANSWER', mergeSort([3, 8, 4, 7, 1, 6, 2, 5, 9, 0])); // Expected: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    // console.log('ANSWER', mergeSort([25, 17, 8, 12, 30, 3, 6, 14, 18, 21, 19])); // Expected: [3, 6, 8, 12, 14, 17, 18, 19, 21, 25, 30]
+    console.log('ANSWER', mergeSort([1,2,3,4,0,5,6,7])); // Expected: 1, 2, 3, 4, 5, 6, 7]
 
 
